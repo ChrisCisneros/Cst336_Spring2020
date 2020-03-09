366dcc5b5f422b0e3ce65dbeb9d8affa287fe691
EC2 Default User <ec2-user@ip-172-31-31-6.us-east-2.compute.internal>
2020-03-09 04:38:46 +0000

    adding lab 5

--- Lab 5/app.js
+++ Lab 5/app.js
@@ -4,7 +4,14 @@ let getImages = function () {
         url:"https://pixabay.com/api/",
         type: "GET",
         dataType: "json",
-        data{
+        data: {
+            "key": "15533843-2c41bd4379d9eedaf9beef9d7",
+            "q":  $("#keyword").val(),
+            "orientation": $("#orientation").val(),
+            "image_type": $("")
+        },
+        success: function(data) {
+         $("#Img1").html("<img src=" + results.id + ">")
             
         }
     })
